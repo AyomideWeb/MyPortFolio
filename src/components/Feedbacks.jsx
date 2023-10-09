410a2aa73daa114a36bbf8604054d9af6d49de85
Akinfenwa Gabriel <gabrielakinfenwa2019@gmail.com>
2023-10-09 17:19:46 +0100

    Updated

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { SectionWrapper } from '../hoc';
+import { fadeIn, textVariant } from '../utils/motion';
+import { testimonials } from '../constants';
+
+
+const FeedbackCard = ({ index, testimonial, name, designation, company, image }) => (
+  <motion.div
+    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+  >
+    <p>{testimonial}</p>
+  </motion.div>
+)
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
-  )
-}
+    <div className='mt-12 bg-black-100 rounded-[20px]'>
+      <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+        </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard
+            key={testimonial.name}
+            index={index}
+            {...testimonial}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Feedbacks
