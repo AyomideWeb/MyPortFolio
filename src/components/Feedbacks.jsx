37a6e5d7958f566de77bc7bb5d305b41897c8cca
Akinfenwa Gabriel <gabrielakinfenwa2019@gmail.com>
2023-10-09 21:47:27 +0100

    Updated

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -10,7 +10,22 @@ const FeedbackCard = ({ index, testimonial, name, designation, company, image })
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
     className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
   >
-    <p>{testimonial}</p>
+    <p className='font-white font-black text-[48px]'>"</p>
+    <div className='mt-1'>
+      <p className='text-white tracking--wider text-[18px]'>{testimonial}</p>
+
+      <div className='mt-7 flex justify-between items-center gap-1'>
+        <div className='flex-1 flex flex-col'>
+          <p className='text-white font-medium text-[16px]'>
+            <span className='blue-text-gradient'>@</span>{name}
+          </p>
+          <p className='mt-1 text-secondary text-[12px]'>
+            {designation} of {company}
+          </p>
+        </div>
+        <img src={image} alt={`feedback-by-${name}`} className='w-10 h-10 rounded-full object-cover' />
+      </div>
+    </div>
   </motion.div>
 )
 
@@ -36,4 +51,4 @@ const Feedbacks = () => {
   );
 };
 
-export default Feedbacks
+export default SectionWrapper(Feedbacks, "");
